1ebdfe13435b326b74bbee7df7081d00b3260abb
jiacheng <s124202468@gmail.com>
2016-01-22 22:32:23 +0800

    add sample data

--- app/models/flow.rb
+++ app/models/flow.rb
@@ -1,2 +1,3 @@
 class Flow < ActiveRecord::Base
+	has_many :steps
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,2 +1,6 @@
 class Step < ActiveRecord::Base
+	belongs_to :flow
+	serialize :items  
+	serialize :next
+	serialize :prev
 end
