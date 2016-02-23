11e8f1d631a8e6c8deb165ec7f5b8d132ab40e2f
jiacheng <s124202468@gmail.com>
2016-02-23 18:58:34 +0800

    git rm -rf --cached .

--- config/settings.rb
+++
@@ -1,6 +0,0 @@
-module Settings
-	NCU_API_TOKEN='f642b6e001a302605d27daf146952fba68c4ffe86758d3b546cbbdae72720aaa49a2d22b67d8ec2e06aa4fe4e384f7429450ebe4ceb3a23dc6499eb28905ce79'
-	OAUTH_TOKEN_URL = 'http://140.115.3.188/oauth/management/v1/api_tokens/token/'
-						
-	OAUTH_ACCESS_TOKEN_URL='http://140.115.3.188/oauth/management/v1/access_tokens/token/'
-end 
